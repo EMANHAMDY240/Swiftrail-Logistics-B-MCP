59e1728b50d17f232f1921807539eb4445e42e5e
Eman Hamdy <emanhamdyabdelaleem@gmail.com>
2026-08-01 02:11:37 +0300

    Verify tools against real MySQL data; use env vars for db credentials

--- mcp_server/db.py
+++ mcp_server/db.py
@@ -1,11 +1,12 @@
+import os
 import pymysql
 import pymysql.cursors
 
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "your_user",
-    "password": "your_password",
-    "database": "swiftrail_db",
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "user": os.environ.get("DB_USER", "root"),
+    "password": os.environ.get("DB_PASSWORD", ""),
+    "database": os.environ.get("DB_NAME", "swiftrail_db"),
     "cursorclass": pymysql.cursors.DictCursor,
 }
 
